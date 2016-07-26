0a149156657dacb5fdcac08c59d834923b046a9f
finalight <finalight@gmail.com>
2016-07-26 22:02:19 +0800

    refactor

--- controllers/inlineModeController.js
+++ controllers/inlineModeController.js
@@ -5,34 +5,19 @@ class InlineModeController extends TelegramBaseInlineQueryController {
 
 	handle($) {
 		console.log($)
-			var results = []
-			for(var i = 0; i < 10; i++){
-			results.push({
-				id: i + "",
-				type: 'gif',
-				gif_url: 'http://thecatapi.com/api/images/get?format=src&size=med&type=gif&uid=' + Math.random().toString(36).substring(2),
-				gif_width: 250,
-				gif_height: 250,
-				thumb_url: 'http://thecatapi.com/api/images/get?format=src&size=small&type=gif&uid=' + Math.random().toString(36).substring(2)
-			});
-			}
-			$.answer(results, 10)		
-		// $.inlineMode(($) => {
-		// 	var results = []
-		// 	for(var i = 0; i < 10; i++){
-		// 	results.push({
-		// 		type: 'gif',
-		// 		gif_url: 'http://thecatapi.com/api/images/get?format=src&size=med&type=gif&uid=' + Math.random().toString(36).substring(2),
-		// 		gif_width: 250,
-		// 		gif_height: 250,
-		// 		thumb_url: 'http://thecatapi.com/api/images/get?format=src&size=small&type=gif&uid=' + Math.random().toString(36).substring(2)
-		// 	});
-		// 	}
-		// 	$.paginatedAnswer(results, 10)		
-		// })
+		var results = []
+		for (var i = 0; i < 10; i++) {
+		results.push({
+			id: i + '',
+			type: 'gif',
+			gif_url: 'http://thecatapi.com/api/images/get?format=src&size=med&type=gif&uid=' + Math.random().toString(36).substring(2),
+			gif_width: 250,
+			gif_height: 250,
+			thumb_url: 'http://thecatapi.com/api/images/get?format=src&size=small&type=gif&uid=' + Math.random().toString(36).substring(2)
+		})
+		}
+		$.answer(results, 10)
     }
-
-
 }
 
 module.exports = InlineModeController
